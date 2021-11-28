a212cf000b00a3c1385d8f7b0f3271ef3d9a8254
DESKTOP-43QG0VI\BINH-DEV <xuanbinh313@gmail.com>
2021-11-28 19:39:26 +0700

    Add constants

---
+++ lib/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFF3E4067);
+const kTextColor = Color(0xFF3F4168);
+const kIconColor = Color(0xFF5E5E5E);
+
+const kDefaultPadding = 20.0;
+
+final kDefaultShadow = BoxShadow(
+  offset: const Offset(3, 3),
+  blurRadius: 10,
+  color: Colors.black.withOpacity(0.16),
+  spreadRadius: -2,
+);
